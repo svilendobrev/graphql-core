60ca4260a9526b68421ac2168fd374d1208a79c6
James Kominick <james@kominick.com>
2018-06-10 23:18:13 -0400

    add UndefinedDefaultValue type

--- graphql/utils/undefined.py
+++ graphql/utils/undefined.py
@@ -9,3 +9,19 @@ class _Undefined(object):
 
 
 Undefined = _Undefined()
+
+
+class _UndefinedDefaultValue(object):
+    def __bool__(self):
+        return False
+
+    __nonzero__ = __bool__
+
+    def __eq__(self, other):
+        return isinstance(other, _UndefinedDefaultValue)
+
+    def __repr__(self):
+        return 'UndefinedDefaultValue'
+
+
+UndefinedDefaultValue = _UndefinedDefaultValue()
